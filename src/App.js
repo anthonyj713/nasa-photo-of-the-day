0b4ee3fa709a6caa3f4a2223f69b469aff34525b
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-22 18:23:16 -0700

    Just getting the project started. Able to pull image from API.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-
+// https://api.nasa.gov/planetary/apod?api_key=erTW5M9qngVOSe2b65T5b9VCOFpVtCw3CoCEeX2N
 function App() {
   return (
     <div className="App">
@@ -8,8 +8,10 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <img src="https://apod.nasa.gov/apod/image/2001/Hyades_Mtanous_1080.jpg"/>
     </div>
   );
 }
 
 export default App;
+
